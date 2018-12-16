6d89d65aeea744c54dfa7e3bb2b86f6a02b2deb1
tzheng24 <tzheng24@wisc.edu>
2018-12-15 21:57:16 -0600

    Add files via upload

--- auctionbase/create_auctionbase/trigger3_add.sql
+++ auctionbase/create_auctionbase/trigger3_add.sql
@@ -5,8 +5,19 @@ PRAGMA foreign_keys = ON;
 drop trigger if exists trigger3;
 
 create trigger trigger3
-	before insert on Bids
-	for each row when (NEW.Amount <= (Select i.Currently from Items i where NEW.ItemID = i.ItemID))
-	begin
-		SELECT raise(rollback, ‘Trigger3_Failed’);
-	end;
+  before insert on Bids
+  for each row
+    when EXISTS (
+        SELECT 1
+        FROM Items i
+        WHERE New.ItemID = i.ItemID
+          AND 
+          (
+              i.Number_of_Bids > 0 AND NEW.Amount <= i.Currently
+              OR
+              i.Number_of_Bids == 0 AND NEW.AMOUNT < i.Currently
+          )
+      )
+  begin
+    SELECT raise(rollback, ‘Trigger3_Failed’);
+  end;
--- auctionbase/create_auctionbase/trigger8_add.sql
+++ auctionbase/create_auctionbase/trigger8_add.sql
@@ -8,5 +8,5 @@ create trigger trigger8
 	after insert on Bids
 	for each row
 	begin
-		UPDATE Items SET Currently = New.Amount;
+		UPDATE Items SET Currently = New.Amount WHERE New.ItemID = Items.ItemID;
 	end;
